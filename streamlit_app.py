b4f29cbd8d70d0893b69682dd8728e3c3322e891
JJRadamantys <jjcasatroa@gmail.com>
2025-07-11 02:45:34 +0000

    release version 1.0

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,9 @@ from google.cloud import firestore
 from google.oauth2 import service_account
 
 import json
-key_dic = json.loads(st.secrets["textkey"])
+#key_dic = json.loads(st.secrets["textkey"])
+key_dic = st.secrets["textkey"]
+
 creds = service_account.Credentials.from_service_account_info(key_dic)
 db = firestore.Client(credentials=creds, project="names-project-demo.json")
 
